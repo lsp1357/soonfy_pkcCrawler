a22bba941d894c818c477d952745f3c67b194c08
soonfy <soonfy@163.com>
2016-09-15 13:47:37 +0800

    crawler

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ let db = 'mongodb://localhost/pkc3038';
 let mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect(db);
-mongoose.set('debug', true);
+// mongoose.set('debug', true);
 
 var app = express();
